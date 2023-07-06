a56c83e5f0e2d030f18c6e566774f7e4323c14c6
righthan <2753875841@qq.com>
2023-07-06 19:58:51 +0800

    图书管理模块

--- src/main/java/com/example/library/Controller/managercontroller/BookController.java
+++ src/main/java/com/example/library/Controller/managercontroller/BookController.java
@@ -1,15 +1,14 @@
 package com.example.library.Controller.managercontroller;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.library.entity.Book;
 import com.example.library.mapper.BookMapper;
 import com.example.library.utils.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DuplicateKeyException;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController()
 @RequestMapping("/book")
@@ -29,35 +28,38 @@ public class BookController {
             return  Result.error().data("info", "ISBN号已存在");
         }catch (Exception e){
             System.out.println(e);
-            return Result.error().data("info", "添加失败,参数错误");
+            return Result.error().data(" info", "添加失败,参数错误");
         }
     }
 
     @DeleteMapping("/deleteBook")
-    public ResponseEntity<String> deleteBook(int id) {
-        int rows = bookMapper.deleteById(id);
-        if (rows > 0) {
-            return ResponseEntity.ok("删除成功");
-        } else {
-            return ResponseEntity.ok("删除失败");
+    public Result  deleteBook(int id) {
+        try{
+            bookMapper.deleteById(id);
+            return Result.ok().data("info", "删除成功");
+        }catch (Exception e){
+            System.out.println(e);
         }
-    }
+        return Result.error().data("info", "读者可能有未归还的借阅记录");
 
+    }
     @PostMapping("/update")
-    public int updateBook(String isbn, String name, float price, String author, String publisher, String createTime,
-                          int borrowNum, String description, int restNum, int total, String place) {
-        int rows = bookMapper.updateBook(isbn, name, price, author, publisher, createTime, borrowNum,
-                description, restNum, total, place);
-        return rows;
-    }
+    public int updateBook(@RequestBody Book book){
+        int i = bookMapper.updateById(book);
+        return i;
 
-    @GetMapping("/searchBook")
-    public Result searchBook(String keywords){
-        QueryWrapper<Book> bookQueryWrapper = new QueryWrapper<>();
-        bookQueryWrapper.like("name", keywords).or().like("author", keywords).or();
-        try {
-            List<Book> books = bookMapper.selectList(bookQueryWrapper);
-            return Result.ok().data("books", books);
+    }
+    @GetMapping("/getBookByCondition")
+    public Result getBookByCondition(String isbn, String name, String author, int current, int size){
+        QueryWrapper<Book> queryWrapper = new QueryWrapper<>();
+        try{
+            queryWrapper.like("isbn", isbn);
+            queryWrapper.like("name", name);
+            queryWrapper.like("author", author);
+            // 设置起始值及每页条数
+            Page<Book> page = new Page<>(current,size);
+            IPage iPage = bookMapper.selectPage(page,queryWrapper);
+            return Result.ok().data("book", iPage);
         }catch (Exception e){
             System.out.println(e);
             return Result.error().data("info", "参数错误");
